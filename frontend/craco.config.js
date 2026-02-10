fba0119e737553ca4fc01d1393195a2e7e1795ff
im <imtiaz911@gmail.com>
2026-02-10 04:21:10 +0000

    Nuclear option - disable all webpack optimization

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,18 +1,14 @@
 const path = require("path");
+
 module.exports = {
   webpack: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
     configure: (webpackConfig) => {
-      const minimizerIndex = webpackConfig.optimization.minimizer.findIndex(
-        (plugin) => plugin.constructor.name === "CssMinimizerPlugin"
-      );
-      
-      if (minimizerIndex !== -1) {
-        webpackConfig.optimization.minimizer.splice(minimizerIndex, 1);
-      }
-      
+      // Nuclear option - disable ALL optimization
+      webpackConfig.optimization.minimize = false;
+      webpackConfig.optimization.minimizer = [];
       return webpackConfig;
     },
   },
